f8db8c70199fb0efc2a187e8732e5c2f602d55c7
AATHEESH007 <150920362+AATHEESH007@users.noreply.github.com>
2026-06-22 08:52:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (74.01%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,16 @@
+def dfs(i,j,m,n,mat):
+    if i > m or j > n:
+        return 0
+    if i == m -1 and j == n - 1:
+        return 1
+    if mat[i][j] != -1:
+        return mat[i][j]
+    mat[i][j] = dfs(i+1,j,m,n,mat) + dfs(i,j+1,m,n,mat)
+    return mat[i][j]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        mat = [[-1]*(n+1) for _ in range(m+1)]
+        return dfs(0,0,m,n,mat)
+        
+        
