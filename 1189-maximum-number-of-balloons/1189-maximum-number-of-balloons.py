a7536f68635a8755987560b661847dd1f149c1af
AATHEESH007 <150920362+AATHEESH007@users.noreply.github.com>
2026-06-22 08:28:04 +0530

    Time: 2 ms (68.66%), Space: 19.4 MB (38.08%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,20 +1,12 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        ct = {'b':0,'a':0,'o':0,'l':0,'n':0}
-        for i in text:
-            if i in 'baloon':
-                if i not in ct:
-                    ct[i] = 1
-                else:
-                    ct[i] += 1
-        ans = float('inf')
-        print(ct)
-        ct['o'] = ct['o'] // 2
-        ct['l'] = ct['l'] // 2
-        print(ct)
-
-        for i in ct:
-            if ans > ct[i]:
-                ans = ct[i]
-        return ans
+        d = defaultdict(int)
+        word = "balloon"
+        for t in text:
+            if t in word:
+                d[t] +=1
         
+        if any(t not in d for t in word):
+            return 0
+        else:
+            return min(d['b'], d['a'], d['l']//2, d['o']//2, d['n'])
